154c22a6b7e7b02d3b17549487843393917b6457
blu-dev <the.blu.dev@gmail.com>
2023-07-09 15:23:58 -0700

    make whole directory patcher

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+use std::{collections::BTreeMap, path::PathBuf};
+
+use clap::Parser;
+use hash40::Hash40;
+use motion_patch::MotionMapExt;
+
+#[derive(Parser, Debug)]
+pub struct Args {
+    #[clap(long)]
+    pub source: PathBuf,
+
+    #[clap(long)]
+    pub target: PathBuf,
+
+    #[clap(long)]
+    pub output: PathBuf,
+
+    #[clap(long)]
+    pub labels: PathBuf,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    Hash40::label_map()
+        .lock()
+        .unwrap()
+        .add_labels_from_path(args.labels)?;
+
+    let mut source_motion_lists = vec![];
+
+    for entry in walkdir::WalkDir::new(&args.source) {
+        let entry = entry?;
+
+        if !entry.file_type().is_file() {
+            continue;
+        }
+
+        if entry.path().file_name().unwrap().to_str().unwrap() == "motion_list.bin" {
+            source_motion_lists.push(entry.path().strip_prefix(&args.source)?.to_path_buf());
+        }
+    }
+
+    for path in source_motion_lists {
+        let costume = path.parent().unwrap();
+        let motion = costume.parent().unwrap();
+        let target_path = args.target.join(&path);
+        if !target_path.exists() {
+            continue;
+        }
+
+        let source = motion_lib::open(args.source.join(&path))?;
+        let target = motion_lib::open(target_path)?;
+
+        let patch = BTreeMap::create(&source, &target);
+
+        let yaml = serde_yaml::to_string(&patch)?;
+        let parent = args.output.join(motion);
+        std::fs::create_dir_all(&parent)?;
+
+        std::fs::write(parent.join("motion_patch.yml"), &yaml)?;
+    }
+
+    Ok(())
+}
